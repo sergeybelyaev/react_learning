5c0f701296a279beb6278f744341fc57c9023c90
Sergey Belyaev <sergey.belyaev@p2h.com>
2018-02-06 11:41:05 +0200

    up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,75 @@
-ReactDOM.render(
+/* Hello World */
+/*ReactDOM.render(
 	<h1>Hello, world!</h1>,
 	document.getElementById('root')
+);*/
+
+
+
+/* Introducing JSX */
+/*function formatName(user) {
+	return user.firstName + ' ' + user.lastName;
+}
+const user = {
+	firstName: 'Harper',
+	lastName: 'Perez'
+};
+const element = (
+	<h1>
+	  Hello, {formatName(user)}!
+	</h1>
 );
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);*/
+
+
+
+/* JSX Represents Objects */
+/*const element = (
+	<h1 className="greeting">
+	  Hello, world!
+	</h1>
+);*/
+/*const element = React.createElement(
+	'h1',
+	{className: 'greeting'},
+	'Hello, world!'
+);
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);*/
+
+
+
+/* Rendering Elements */
+/* Rendering an Element into the DOM */
+/*const element = <h1>Hello, world</h1>;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);*/
+/* Updating the Rendered Element */
+/*function tick() {
+	const element = (
+	  <div>
+		<h1>Hello, world!</h1>
+		<h2>
+			It is{' '}
+			{new Date().toLocaleTimeString()}.
+		</h2>
+	  </div>
+	);
+	ReactDOM.render(
+		element,
+		document.getElementById('root')
+	);
+}
+setInterval(tick, 1000);*/
+
+
+
+/* Components and Props */
+/* Functional and Class Components */
